82159e534b160e6028f4a7c490ce11ce16856fbe
unknown <warner@WAM20160717.mit.edu>
2021-01-20 15:24:14 -0500

    Trying multiple times to connect.

--- NetworkRecieve.py
+++ NetworkRecieve.py
@@ -11,28 +11,29 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host_Address, port))
 s.listen(backlog)
 
-try:
-    client, address = s.accept()
-    while True:
-        data = client.recv(size).decode()
-        if data:
-            #print(data)
-            str_data = data.split()
-            drive_val = float(str_data[0])
-            steer_val = float(str_data[1])
-            drive.set_pwm(int(100*drive_val))
-            steering.set_pwm(int(100*steer_val))
-            t_last = time.time()
-            #client.send(data)
-        elif time.time() - t_last > 0.5:
-            drive.set_pwm(0)
-            steering.set_pwm(0)
-            #break
+while True:
+	try:
+	    client, address = s.accept()
+	    while True:
+	        data = client.recv(size).decode()
+	        if data:
+	            print(data)
+	            str_data = data.split()
+	            drive_val = float(str_data[0])
+	            steer_val = float(str_data[1])
+	            drive.set_pwm(int(100*drive_val))
+	            steering.set_pwm(int(100*steer_val))
+	            t_last = time.time()
+	            #client.send(data)
+	        elif time.time() - t_last > 0.5:
+	            drive.set_pwm(0)
+	            steering.set_pwm(0)
+	            #break
 
-except:	
-    print("Closing socket")	
-    client.close()
-    s.close()
+#except:	
+    #print("Closing socket")	
+    #client.close()
+    #s.close()
 
 drive.set_pwm(0)
 steering.set_pwm(0)
