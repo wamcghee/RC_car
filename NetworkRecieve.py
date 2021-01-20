b594040dc69eb92892e4eea0a3e373fbbac34074
unknown <warner@WAM20160717.mit.edu>
2021-01-20 15:27:45 -0500

    Remove try/except to see error message.

--- NetworkRecieve.py
+++ NetworkRecieve.py
@@ -11,27 +11,27 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host_Address, port))
 s.listen(backlog)
 
+#while True:
+	#try:
+client, address = s.accept()
 while True:
-	try:
-	    client, address = s.accept()
-	    while True:
-	        data = client.recv(size).decode()
-	        if data:
-	            print(data)
-	            str_data = data.split()
-	            drive_val = float(str_data[0])
-	            steer_val = float(str_data[1])
-	            drive.set_pwm(int(100*drive_val))
-	            steering.set_pwm(int(100*steer_val))
-	            t_last = time.time()
-	            #client.send(data)
-	        elif time.time() - t_last > 0.5:
-	            drive.set_pwm(0)
-	            steering.set_pwm(0)
+    data = client.recv(size).decode()
+    if data:
+        print(data)
+        str_data = data.split()
+        drive_val = float(str_data[0])
+        steer_val = float(str_data[1])
+        drive.set_pwm(int(100*drive_val))
+        steering.set_pwm(int(100*steer_val))
+        t_last = time.time()
+        #client.send(data)
+    elif time.time() - t_last > 0.5:
+        drive.set_pwm(0)
+        steering.set_pwm(0)
 	            #break
 
-	except:
-		print('Failed to accept connection.')	
+	#except:
+		#print('Failed to accept connection.')	
 	    #print("Closing socket")	
 	    #client.close()
 	    #s.close()
