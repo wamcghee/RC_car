665fd888716617d0b7f1a40ee6629db6ec6d1a56
unknown <warner@WAM20160717.mit.edu>
2021-01-25 14:25:12 -0500

    Forgot self.

--- Server.py
+++ Server.py
@@ -20,7 +20,7 @@ class Server:
             self.socket.bind((self.server_IP, self.port))
             self.socket.listen(self.backlog)
             self.listen()
-            self.client, self.client_address = socket.accept()
+            self.client, self.client_address = self.socket.accept()
             self.connected = True
             self.t_last = time.time()
             self.display = ('Connection made.\n' +
